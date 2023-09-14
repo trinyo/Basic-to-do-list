4fbc311bd137a04075bba3d4100b3e492bf323e8
Tritya-git <76614642+Trinyo@users.noreply.github.com>
2023-09-14 11:46:18 +0200

    chore: :construction: basic layout system

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
 function App() {
   return (
-    <div className="h-screen flex bg-gradient-to-r from-purple-500 to-pink-500"></div>
+    // main container
+    <div className="h-screen p-1  flex flex-row gap-2 bg-gradient-to-r from-purple-500 to-pink-500">
+      {/* Side bar */}
+      <div className="h-full w-16 backdrop-blur-sm bg-white/80 rounded"></div>
+      {/* Right content container */}
+      <div className="flex flex-col gap-2 flex-1">
+        {/* Top bar */}
+        <div className="h-12 w-full rounded backdrop-blur-sm bg-white/80"></div>
+        <div className="flex-1 backdrop-blur-sm bg-white/80 rounded"></div>
+      </div>
+    </div>
   );
 }
 
